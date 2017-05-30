c7e18f411cad8c1ca74e285fe877ed4ae7fff69d
Viktor Kotusenko <roxolanus@gmail.com>
2017-05-30 09:23:32 -0400

    move expectations to dev dependencies

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [expectations "2.2.0-beta1"]
+  :dependencies [[org.clojure/clojure "1.8.0"]                 
                  [marginalia "0.9.0"]]
   :main ^:skip-aot discerno.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:dev {:dependencies [[expectations "2.2.0-beta1"]]}
+             :uberjar {:aot :all}})
