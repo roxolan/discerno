2845640658290669d215f306cd20f7462381097d
Viktor Kotusenko <roxolanus@gmail.com>
2017-05-30 23:42:45 -0400

    enable logging

--- src/discerno/core.clj
+++ src/discerno/core.clj
@@ -1,10 +1,12 @@
 (ns discerno.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.tools.logging :as log]))
 
 (defn -main
   "I don't do a whole lot."
   [& args]
-  (println "Salve Mundus!"))
+  (println "Salve Mundus!")
+  (log/info "logging works!"))
 
 (defn mult3
   [x]
--- test/discerno/core_test.clj
+++ test/discerno/core_test.clj
@@ -1,5 +1,7 @@
 (ns discerno.core-test
-  (:use expectations))
+  (:use expectations)
+  (:require [clojure.tools.logging :as log]))
 
 ;; Math check
 (expect 2 (+ 1 1))
+(log/info "logging works from a test file")
