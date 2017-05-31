bcfcca999d110b33c1283ceee64b2d16f57be75e
Viktor Kotusenko <roxolanus@gmail.com>
2017-05-30 21:36:00 -0400

    update description & dependencies

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
-(defproject discerno "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject discerno "discerno-0.1.0-SNAPSHOT"
+  :description "Personal Learning Environment"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]                 
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [environ "1.1.0"]
                  [marginalia "0.9.0"]]
   :main ^:skip-aot discerno.core
   :target-path "target/%s"
